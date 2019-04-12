7ec3a528b55422302e785e84b31f0f5d8e224d0c
Karenna Zoss <karennarachelle@gmail.com>
2019-04-11 20:05:56 -0600

    transfered everything over

--- kirby.js
+++ kirby.js
@@ -1,7 +1,8 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "25%";
+    document.getElementById("mySidenav").style.width = "30%";
 }
-
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+// SOME STUFF TO TEST A GITHUB PUSH
+// ANOTHER TEST PUSH
